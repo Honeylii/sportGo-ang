a0c2b13144d7b2c56400f32915cacf9f9ad96ca0
Soner Mezgitci <sonermezgitci@Soners-MBP.lan>
2022-06-16 15:38:59 -0400

    First Commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { PlayerProfileComponent } from './../player-profile/player-profile.component';
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

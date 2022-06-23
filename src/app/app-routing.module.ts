dea898f21634483a4e213e75df0f1d0374bc5938
sonermezgitci <mezgitci9@gmail.com>
2022-06-23 17:30:46 -0400

    Login page implemented / All routes works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,24 @@
-
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';  
-import { RegisterComponent } from './Auth/register/register.component'; 
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './Auth/register/register.component';
+import { LoginComponent } from './Auth/login/login.component';
 import { TestStateComponent } from './Auth/test-state/test-state.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'auth/register', component: RegisterComponent},
-  {path: 'tests', component: TestStateComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'auth/register', component: RegisterComponent },
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'tests', component: TestStateComponent },
   // {path: 'contacts/edit/:contactId', component: EditContactComponent},
   // {path: 'contacts/view/:contactId', component: ViewContactComponent},
-  {path: '**', component: PageNotFoundComponent},
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
